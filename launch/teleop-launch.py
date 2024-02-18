fc440def4b175a07cccb1f1756bd939ab4c24774
Bruce Digney <bdigney@shaw.ca>
2024-02-18 16:23:27 -0700

    changed topics

--- launch/teleop-launch.py
+++ launch/teleop-launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
     config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
+        launch.actions.DeclareLaunchArgument('joy_vel', default_value='/diffbot_base_controller/cmd_vel_unstamped'),
         launch.actions.DeclareLaunchArgument('joy_config', default_value='ps3'),
         launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
